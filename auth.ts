4093da0ce2444333108720cc6b56e406e9f3f333
Boris Sobur <59290755+soburboris@users.noreply.github.com>
2021-12-11 11:29:16 +0600

    add !!! in conflict-bransh

--- auth.ts
+++ auth.ts
@@ -1,7 +1,7 @@
 function auth (login: number | string, password: number | string) {
 	if ( login == 1212 && password == 'efeferfe') {
-		return "We are logged"
+		return "We are logged!!!"
 	} else {
-		return "Login or password is incorrect"
+		return "Login or password is incorrect!!!"
 	}
 }
